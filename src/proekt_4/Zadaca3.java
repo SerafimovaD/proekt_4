eb1984659f39341044d60e077fae6c3281348381
SerafimovaD <daci.serafimova@gmail.com>
2023-11-21 13:18:47 +0100

    Add first java program

---
+++ src/proekt_4/Zadaca3.java
@@ -0,0 +1,26 @@
+package proekt_4;
+
+import java.util.Scanner;
+
+public class Zadaca3 {
+
+	public static void main(String[] args) {
+		int x=1, y=1, z=1;
+		if(x>y || z>y) 
+			System.out.print("1");
+	    else 
+			System.out.print("2");
+		if(Math.abs(x-y)>=z)
+		if(x>y)
+		System.out.print("3");
+		else 
+		System.out.print("4");
+		else 
+		System.out.print("5");
+		
+		}
+		
+
+	}
+
+
